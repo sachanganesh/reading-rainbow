ab97193eda2285f4d6bd20aa0e6212b1fb0260f3
Jimmy Liu <sicong.liu98@gmail.com>
2015-02-08 10:47:28 -0800

    sachan

--- app/js/main.js
+++ app/js/main.js
@@ -20,8 +20,27 @@
 	scotchApp.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('homeController', function($scope, $location) {
+	scotchApp.controller('homeController', function($scope, $location, $http) {
 		// create a message to display in our view
+		$scope.search = function () {
+			$http.post('http://reading-rainbow.herokuapp.com/api/search', {msg:$scope.query}).
+			  success(function(data, status, headers, config) {
+			    console.log(data);
+			  }).
+			  error(function(data, status, headers, config) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			  });
+		}
+		$http.get('http://reading-rainbow.herokuapp.com/api/popular').
+		  success(function(data, status, headers, config) {
+		   	$scope.popular = data;
+		   	//console.log(data);
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
 	  $scope.message = 'Everyone come and see how good I look!';
 	  $scope.query;
 	  $scope.request = require('request');
@@ -36,7 +55,8 @@
 	      });
 	    });
 	  }
-	  $scope.download = function (magnetUri) {
+	  $scope.download = function () {
+	  	console.log(magnetUri);
 	    var WebTorrent = require('webtorrent');
 	    var client = new WebTorrent();
 	    var pdf;
@@ -50,7 +70,7 @@
 			    source.pipe(destination);
 			    console.log(destination);
 			    console.log("done");
-			    //$location.url('/read');
+			    //$location.url('read.html');
 	      	}
 	      });
 	    });
@@ -77,37 +97,124 @@
 	  }
 	});
 
-	scotchApp.controller('bookController', function($scope) {
+	scotchApp.controller('bookController', function($scope, $location, $http) {
 		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
+		var absUrl = $location.absUrl();
+		$scope.search = absUrl.substring(absUrl.search('search=') + 7);
+		console.log($scope.search);
+		$http.post('http://reading-rainbow.herokuapp.com/api/search', {query:$scope.search}).
+			  success(function(data, status, headers, config) {
+			  	$scope.book = data;
+			    console.log(data);
+			  }).
+			  error(function(data, status, headers, config) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+		});
+			  $scope.query;
+	  $scope.request = require('request');
+	  $scope.Q = require('q');
+	  $scope.fs = require('fs');
+	  $scope.kickass = require('kickass-search');
+	  $scope.stream = function () {
+	    $scope.kickass.search('ebooks', 'great gatsby').then(function (data) {
+	      $scope.mostSeeders(data).then(function (torrent) {
+	      	console.log(JSON.stringify(torrent));
+	      	$scope.download(torrent.magnet.href);
+	      });
+	    });
+	  }
+	  $scope.download = function () {
+	    var WebTorrent = require('webtorrent');
+	    var client = new WebTorrent();
+	    var pdf;
+	    console.log($scope.book.magnet_link);
+	    client.download($scope.book.magnet_link, function (torrent) {
+	      console.log('Torrent info hash:', torrent.infoHash);
+	      torrent.files.forEach(function (file) {
+	      	console.log(file.name);
+	      	if (file.name.indexOf("epub") >= 0) {
+		        var source = file.createReadStream();
+			    var destination = $scope.fs.createWriteStream(file.name);
+			    source.pipe(destination);
+			    console.log(destination);
+			    console.log("done");
+			    //$location.path('read.html?name=' + file.name);
+			    $(".hello").append('<a href="read.html?name=' + file.name + '"><button class="fuckme">Hi</button></a>');
+			    $( ".fuckme" ).click();
+			    //$location.path('read.html?name=' + file.name);
+			    //$location.replace();
+	      	}
+	      });
+	    });
+	  }
+	  $scope.mostSeeders = function (torrents) {
+	    if (!torrents || torrents.length == 0) {
+	      console.log('ERROR');
+	      return null;
+	    }
+	    var deferred = $scope.Q.defer();
+	    $scope.compareSeeds(torrents).then(function (data) {
+	      deferred.resolve(data);
+	    });
+	    return deferred.promise;
+	  }
+	  $scope.compareSeeds = function (torrents) {
+	    var deferred = $scope.Q.defer();
+	    var torrent = torrents[0];
+	    for (var i = 1; i < torrents.length; i++)
+	      if (parseInt(torrent.seed) < parseInt(torrents[i].seed))
+	        torrent = torrents[i];
+	    deferred.resolve(torrent);
+	    return deferred.promise;
+	  }
 	});
 
-	scotchApp.controller('readController', function($scope) {
-		// create a message to display in our view
-		outerScope = $scope
-		$scope.readEPub = function (file) {
-			var EPub = require('epub');
-			var epub = new EPub('test.epub',  '/imagewebroot/', '/articlewebroot/');
-			epub.on("error", function(err){
-				console.log('ERROR\n-----');
-				throw err;
-			});
+	scotchApp.controller('readController', function($scope, $location) {
+		$scope.Q = require('q');
 
-			epub.on('end', function () {
-			    	epub.getChapter(epub.spine.contents[4].id, function(err, data){
-					outerScope.text = "aa"
-			        if(err){
-			            console.log(err);
-			            return;
-			        }
-			        console.log("\nFIRST CHAPTER:\n");
-			        outerScope.text = data;
-			        outerScope.$apply()
-			        console.log(outerScope.text);
-			        console.log(data.substr(0,512)+"..."); // first 512 byte
+		$scope.page = 0;
+
+		outerScope = $scope;
+
+		// create a message to display in our view
+		$scope.readEPub = function (file) {
+			function setFoundation() {
+				var deferred = $scope.Q.defer();
+				var absUrl = $location.absUrl();
+				$scope.bookName = absUrl.substring(absUrl.search('name=') + 5);
+				var temp = absUrl.substring(absUrl.search('name=') + 5);
+				temp = decodeURI(temp)
+				console.log(temp);
+				$scope.bookName = temp;
+				console.log($scope.bookName);
+				deferred.resolve($scope.bookName);
+				return deferred.promise;
+			}
+			setFoundation().then(function (bookName) {
+				var EPub = require('epub');
+				var epub = new EPub(bookName, '/imagewebroot/', '/articlewebroot/');
+				epub.on("error", function(err){
+					console.log('ERROR\n-----');
+					throw err;
+				});
+
+				epub.on('end', function () {
+				    	epub.getChapter(epub.spine.contents[$scope.page].id, function(err, data){
+						outerScope.text = "aa"
+				        if(err){
+				            console.log(err);
+				            return;
+				        }
+				        console.log("\nFIRST CHAPTER:\n");
+				        outerScope.text = data;
+				        outerScope.$apply()
+				        console.log(outerScope.text);
+				        console.log(data.substr(0,512)+"..."); // first 512 byte
+				    });
 			    });
-		    });
-			epub.parse();
+				epub.parse();
+			});
 		}
 
 		$scope.readEPub();
